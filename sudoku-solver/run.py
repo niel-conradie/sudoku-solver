1b68f7c839c818f68d9b412a16faefec50f7e093
Niel Conradie <nielconradie93@gmail.com>
2022-06-07 14:17:56 +0200

    Create run.py
    Added run.py to start the program.

---
+++ sudoku-solver/run.py
@@ -0,0 +1,30 @@
+from sudoku import SudokuSolver
+
+
+def run():
+    """ Sudoku Solver. """
+    run = SudokuSolver()
+
+    # Starting sudoku.
+    sudoku = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ]
+
+    # Solve sudoku.
+    run.solver(sudoku)
+    # Display solved sudoku.
+    print(sudoku)
+
+
+if __name__ == '__main__':
+    run()

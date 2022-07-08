561f537162ec19bfd119ea22c30bbac0061bbc53
Niel Conradie <nielconradie93@gmail.com>
2022-07-08 09:41:28 +0200

    Update run.py
    Added new method to run.py file.

--- sudoku-solver/run.py
+++ sudoku-solver/run.py
@@ -5,26 +5,31 @@ def run():
     """Sudoku Solver."""
     run = SudokuSolver()
 
-    # Starting empty sudoku.
-    sudoku = run.empty_sudoku()
-
     while True:
-        # Display sudoku board locations.
+        # Starting empty sudoku.
+        sudoku = run.empty_sudoku()
+
+        while True:
+            # Display sudoku board locations.
+            run.display_sudoku(sudoku)
+            # Requesting user input.
+            user_input = run.user_input_location()
+            # User input continue condition.
+            if user_input == 'done':
+                break
+            # Assign user input to appropriate location.
+            run.user_input_allocation(sudoku, user_input)
+
+            continue
+
+        # Solve sudoku.
+        run.solver(sudoku)
+        # Display solved sudoku.
         run.display_sudoku(sudoku)
         # Requesting user input.
-        user_input = run.user_input_location()
-        # User input continue condition.
-        if user_input == 'done':
-            break
-        # Assign user input to appropriate location.
-        run.user_input_allocation(sudoku, user_input)
+        run.restart()
 
         continue
-
-    # Solve sudoku.
-    run.solver(sudoku)
-    # Display solved sudoku.
-    run.display_sudoku(sudoku)
 
 
 if __name__ == '__main__':

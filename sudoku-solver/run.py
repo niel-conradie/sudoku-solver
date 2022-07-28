9f19faa75be141e778910a468bb12af40ca437ea
Niel Conradie <nielconradie93@gmail.com>
2022-07-28 15:31:36 +0200

    Update run.py
    Removed unused old code and added start_app() method.

--- sudoku-solver/run.py
+++ sudoku-solver/run.py
@@ -5,32 +5,9 @@ def run():
     """Sudoku Solver."""
     run = SudokuSolver()
 
-    while True:
-        # Starting empty sudoku.
-        sudoku = run.empty_sudoku()
-
-        while True:
-            # Display sudoku board locations.
-            run.display_sudoku(sudoku)
-            # Requesting user input.
-            user_input = run.user_input_location()
-            # User input continue condition.
-            if user_input == 'done':
-                break
-            # Assign user input to appropriate location.
-            run.user_input_allocation(sudoku, user_input)
-
-            continue
-
-        # Solve sudoku.
-        run.solver(sudoku)
-        # Display solved sudoku.
-        run.display_sudoku(sudoku)
-        # Requesting user input.
-        run.restart()
-
-        continue
+    # starting the application.
+    run.start_app()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     run()

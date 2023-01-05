8187e3f992fa92a7ac110600de293d230922f6ac
Niel Conradie <nielconradie93@gmail.com>
2023-01-05 12:02:06 +0200

    Update run.py
    Added Keyboard Interrupt exception handeling

--- sudoku-solver/run.py
+++ sudoku-solver/run.py
@@ -5,8 +5,12 @@ def run():
     """Sudoku Solver."""
     run = SudokuSolver()
 
-    # starting the application.
-    run.start_app()
+    try:
+        # Starting the application.
+        run.start_app()
+    except KeyboardInterrupt:
+        # Stopping the application.
+        exit("\n\nProgram Terminated")
 
 
 if __name__ == "__main__":

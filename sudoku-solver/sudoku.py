4e2107b76b0168ee1490b7803168e52619b4aa48
Niel Conradie <nielconradie93@gmail.com>
2023-01-09 13:59:28 +0200

    Update sudoku.py
    Updated comments

--- sudoku-solver/sudoku.py
+++ sudoku-solver/sudoku.py
@@ -5,20 +5,24 @@ class SudokuSolver:
     def user_input_location():
         """Requesting user input and validating choice."""
         while True:
+            # Requesting user input.
             user_input = input(
                 "\nType 'done' to solve sudoku.\n\nEnter location: "
             ).lower()
-            choices = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9',
-                        'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9',
-                        'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9',
-                        'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9',
-                        'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'e9',
-                        'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
-                        'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9',
-                        'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9',
-                        'i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8', 'i9',
-                        'done'
-                        ]
+
+            # User input validation conditions.
+            choices = [
+                "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9",
+                "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b9",
+                "c1", "c2", "c3", "c4", "c5", "c6", "c7", "c8", "c9",
+                "d1", "d2", "d3", "d4", "d5", "d6", "d7", "d8", "d9",
+                "e1", "e2", "e3", "e4", "e5", "e6", "e7", "e8", "e9",
+                "f1", "f2", "f3", "f4", "f5", "f6", "f7", "f8", "f9",
+                "g1", "g2", "g3", "g4", "g5", "g6", "g7", "g8", "g9",
+                "h1", "h2", "h3", "h4", "h5", "h6", "h7", "h8", "h9",
+                "i1", "i2", "i3", "i4", "i5", "i6", "i7", "i8", "i9",
+                "done",
+            ]
             if user_input not in choices:
                 print(f"\n{user_input} is not an valid choice!")
                 continue
@@ -29,12 +33,14 @@ class SudokuSolver:
     def user_input_number():
         """Requesting user input and validating choice."""
         while True:
+            # Requesting user input.
             try:
                 user_input = int(input("\nEnter number: "))
             except ValueError:
                 print("\nThat is not a number.")
                 continue
 
+            # User input validation conditions.
             choices = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
             if user_input not in choices:
                 print(f"\n{user_input} is not an valid choice!")
@@ -342,10 +348,12 @@ class SudokuSolver:
     def restart():
         """Requesting user input and validating choice."""
         while True:
+            # Display user input options.
             print("\nTry Again?")
             print("\nYes: Type '1'")
             print("No: Type '2'")
 
+            # Requesting user input.
             try:
                 user_input = int(input("\nEnter: "))
             except ValueError:
